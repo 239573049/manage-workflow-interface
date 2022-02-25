07419ce247875070d4af684605e5c30b1a4b75d0
239573049 <239573049@qq.com>
2022-02-26 03:12:42 +0800

    xg

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Layout,Row,Col} from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <Header>
+                        1
+                    </Header>
+                    <Content>
+                        
+                        <Row>
+                        <Col span={12} offset={6}>
+                            col-12 col-offset-6
+                        </Col>
+                        </Row>
+                    </Content>
+                    <Footer>
+                        
+                    </Footer>
+                </Layout>
+            </div>
+        )
+    }
+}
+
+export default Login;
