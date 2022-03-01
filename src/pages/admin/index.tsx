1632bfa95408185c58cdc5720dcca0c6e3791746
239573049 <239573049@qq.com>
2022-03-02 01:31:57 +0800

    更改标签页

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,32 +1,94 @@
 import React from 'react'
-import { Layout, Menu, Image, Popover, message } from 'antd';
+import { Layout, Menu, Image, Popover, message, Card, Avatar, Tabs } from 'antd';
 import { UserInfo } from '../../model/userInfo/userInfo';
 import './index.less'
 import AdminApi from '../../apis/admin/index'
 import { Response } from '../../model/request/Api'
 import { LoginVM } from '../../model/login/login';
-import Work from './work';
-import Home from './home/index'
-import User from './user/index'
-import WorkConfig from './system/workConfig';
-import Error404 from '../error404';
-import RoleConfig from './system/roleConfig/index'
-import UserConfig from './system/userConfig/index';
-import LoginApi from '../../apis/login/index'
+import LoginApi from '../../apis/login/index';
+import PANE from '../menu';
 const { Header, Content, Footer, Sider } = Layout;
+const { TabPane } = Tabs;
 const { SubMenu } = Menu;
 
+interface IState {
+  menu: any[],
+  panes: {
+    title: string, content: string, key: string
+  }[],
+  activeKey: string;
+  collapsed: boolean,
+  searchValue: string;
+  user: null | UserInfo
+}
+interface IProps {
 
-class Admin extends React.Component {
+}
+
+class Admin extends React.Component<IProps, IState> {
+  state: IState = {
+    menu: [],
+    panes: [],
+    activeKey: "",
+    collapsed: false,
+    searchValue: "",
+    
+    user: new UserInfo()
+  };
+  onChange = (activeKey: string) => {
+    this.setState({ activeKey });
+  };
+
+  onEdit = (targetKey: any, action: "add" | "remove") => {
+    if (action === "remove" && this.state.panes.length === 1) {
+      message.warning("标签页不能小于一个");
+      console.log("a");
+      return;
+    } else if (action === "remove") {
+      this.remove(targetKey);
+    }
+  };
+
+  add = (title: string, content: string, key: string) => {
+    // const { panes } = this.state;
+    const panes = this.state.panes
+      const activeKey = panes && panes.find(a => a.key === key);
+    if (activeKey ) {
+      this.setState({ activeKey:activeKey.key });
+    } else {
+      panes.push({ title: title, content: content, key: key });
+      this.setState({ panes:[...panes], activeKey:key });
+    }
+  };
+
+  remove = (targetKey: string) => {
+    let { activeKey } = this.state;
+    let lastIndex = 0;
+    this.state.panes.forEach((pane, i) => {
+      if (pane.key === targetKey) {
+        lastIndex = i - 1;
+      }
+    });
+    const panes = this.state.panes.filter(pane => pane.key !== targetKey);
+    if (panes.length && activeKey === targetKey) {
+      if (lastIndex >= 0) {
+        activeKey = panes[lastIndex].key;
+      } else {
+        activeKey = panes[0].key;
+      }
+    }
+    this.setState({ panes, activeKey });
+  };
   /**
    * 获取当前账号权限菜单
    */
   getMenu() {
     AdminApi.GetUserMenuList()
       .then(res => {
-        var data = res.data as Response<LoginVM>
-        console.log(data.data);
+        var data = res.data as Response<any[]>
         if (data.statusCode === 200) {
+          var menu=data.data[0];
+          this.add(menu.title,menu.component,menu.key)
           this.setState({
             menu: this.getMenuNodes(data.data as never)
           })
@@ -49,30 +111,8 @@ class Admin extends React.Component {
    * @param item 
    */
   menuOnClick(item: any) {
-    let components = <Error404 />
-    switch (item.component) {
-      case "User":
-        components = <User />
-        break;
-      case "Home":
-        components = <Home />
-        break;
-      case "Work":
-        components = <Work />
-        break;
-      case "WorkConfig":
-        components = <WorkConfig />
-        break;
-      case "RoleConfig":
-        components = <RoleConfig />
-        break;
-      case "UserConfig":
-        components = <UserConfig />
-        break;
-    }
-    this.setState({
-      component: components
-    })
+    console.log(item);
+    this.add(item.title, item.component, item.key)
   }
   getMenuNodes = (menuList: any[]) => {
     return menuList.map(item => {
@@ -92,35 +132,29 @@ class Admin extends React.Component {
       }
     })
   }
-  state = {
-    menu: [],
-    collapsed: false,
-    searchValue: "",
-    component: <Home />,
-    user: new UserInfo()
-  };
 
   onCollapse = (collapsed: any) => {
     console.log(collapsed);
     this.setState({ collapsed });
   };
-  logOut(){
+  logOut() {
     window.sessionStorage.removeItem("user")
     window.sessionStorage.removeItem("token")
     LoginApi.LogOut()
-      .then(res=>{
+      .then(res => {
         message.success("退出登录成功")
-        window.location.href="/"
+        window.location.href = "/"
       })
   }
 
+
   render(): React.ReactNode {
-    const { collapsed, menu, component, user } = this.state;
+    const { collapsed, menu, user } = this.state;
     const content = (
       <div>
-        <p className="information">个人信息</p>
-        <p className="information">消息提醒</p>
-        <p onClick={this.logOut} className="information">退出登录</p>
+        <Card hoverable size="small">个人信息</Card>
+        <Card hoverable size="small">消息提醒</Card>
+        <Card hoverable size="small" onClick={this.logOut} >退出登录</Card>
       </div>
     );
     return (
@@ -138,16 +172,29 @@ class Admin extends React.Component {
           <Header className="site-layout-background" style={{ padding: 0 }} >
             <div
               className='headPortraits'>
+              <span>
                 <Popover content={content} >
-              <Image
-                width={50}
-                preview={false}
-                src={user.headPortraits}
-              /></Popover>
+                  <Avatar size="large" src={user?.headPortraits|| ''} /></Popover>
+              </span>
+              <span>
+                {user?.name || ''}
+              </span>
             </div>
           </Header>
           <Content style={{ margin: '0 16px' }}>
-            {component}
+            <Tabs
+              hideAdd
+              onChange={this.onChange}
+              activeKey={this.state.activeKey}
+              type="editable-card"
+              onEdit={this.onEdit}
+            >
+              {this.state.panes.map(pane => (
+                <TabPane tab={pane.title} key={pane.key}>
+                  {PANE[pane.content]}
+                </TabPane>
+              ))}
+            </Tabs>
           </Content>
           <Footer style={{ textAlign: 'center' }}>Token ©2022 </Footer>
         </Layout>
---
+++ src/pages/menu/index.tsx
@@ -0,0 +1,17 @@
+
+import Work from '../admin/work';
+import Home from '../admin/home/index'
+import User from '../admin/user/index'
+import WorkConfig from '../admin/system/workConfig';
+import Error404 from '../error404';
+import RoleConfig from '../admin/system/roleConfig/index'
+import UserConfig from '../admin/system/userConfig/index';
+interface IG {
+    [propName: string]: JSX.Element;
+}
+
+const AD: IG =  {
+    Work: <Work />, Home: <Home />, User: <User />, WorkConfig: <WorkConfig />, Error404: <Error404 />, RoleConfig: <RoleConfig />, UserConfig: <UserConfig />,
+}
+
+export default AD;
