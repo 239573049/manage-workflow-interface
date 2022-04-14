c6738e749962a360b290e385c5db29b3426f0e1e
239573049@qq.com <239573049@qq.com>
2022-04-14 11:08:04 +0800

    修改配置读取

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 const config={
-    // baseURL:"http://124.222.27.83:8889/api",
-    baseURL:"https://localhost:8088/api",
-    
+    baseURL:"http://124.222.27.83:8889/api",
+}
+if(process.env.NODE_ENV==='development'){
+    config.baseURL="https://localhost:8088/api"
 }
 export default config;
